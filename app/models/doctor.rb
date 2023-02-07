3fc941124dfca331533efbe80e9c93dd236e8e4d
Christian Bongard <christian.bongard90@gmail.com>
2023-02-07 13:09:25 +0400

    add validation for last name presence

--- app/models/doctor.rb
+++ app/models/doctor.rb
@@ -2,4 +2,6 @@ class Doctor < ActiveRecord::Base
   has_many :interns # a doctor can have many Intern instances
   has_many :consultations # a doctor can have many Consultation instances
   has_many :patients, through: :consultations
+
+  validates :last_name, presence: true
 end
