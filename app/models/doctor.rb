77f3687711a6b53487eba9462f6dd6a092fd417f
Christian Bongard <christian.bongard90@gmail.com>
2023-02-07 13:02:55 +0400

    added patients through consultations for doctors

--- app/models/doctor.rb
+++ app/models/doctor.rb
@@ -1,4 +1,5 @@
 class Doctor < ActiveRecord::Base
   has_many :interns # a doctor can have many Intern instances
   has_many :consultations # a doctor can have many Consultation instances
+  has_many :patients, through: :consultations
 end
